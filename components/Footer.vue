775f023c534e6f681db3c18435f88bd716daaae2
kurooooon <b1006110@gmail.com>
2020-05-05 23:42:04 +0900

    add: font awesome, social links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,15 +1,46 @@
 <template>
   <footer class="footer">
+    <ul class="icons">
+      <li>
+        <a target="_blank" href="https://twitter.com/0x000000_net">
+          <font-awesome-icon :icon="twitter" />
+        </a>
+      </li>
+      <li>
+        <a target="_blank" href="https://soundcloud.com/kurooooon">
+          <font-awesome-icon :icon="soundCloud" />
+        </a>
+      </li>
+      <li>
+        <a href="mailto:0x000000.official@gmail.com">
+          <font-awesome-icon :icon="mail" />
+        </a>
+      </li>
+    </ul>
     <p class="footer-text text-center">©2020 0x000000,Inc.</p>
   </footer>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faTwitter, faSoundcloud } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+export default Vue.extend({
+  components: {
+    FontAwesomeIcon
+  },
+  data() {
+    return {
+      soundCloud: faSoundcloud,
+      twitter: faTwitter,
+      mail: faEnvelope
+    }
+  }
+})
 </script>
 
-<style>
+<style lang="scss">
 .footer {
   background-color: var(--primary);
   padding: 3rem 1.5rem;
@@ -17,5 +48,25 @@ export default Vue.extend({})
 }
 .footer-text {
   margin: 0;
+}
+.icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  list-style: none;
+
+  li {
+    display: inline-block;
+    margin-right: 1rem;
+
+    a {
+      color: inherit;
+    }
+
+    &:last-child {
+      margin: 0;
+    }
+  }
 }
 </style>
